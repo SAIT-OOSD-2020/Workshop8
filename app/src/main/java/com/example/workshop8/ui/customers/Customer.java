64631148b88884752971633c8a842098f2fa64ab
tlicudine <74613058+tlicudine@users.noreply.github.com>
2021-05-10 10:05:29 -0600

    customerfragment changes

--- app/src/main/java/com/example/workshop8/ui/customers/Customer.java
+++ app/src/main/java/com/example/workshop8/ui/customers/Customer.java
@@ -4,7 +4,9 @@ import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
-public class Customer extends ViewModel {
+import java.io.Serializable;
+
+public class Customer extends ViewModel implements Serializable {
 
     private int CustomerId;
     private String CustFirstName;
@@ -19,14 +21,117 @@ public class Customer extends ViewModel {
     private String CustEmail;
     private Integer AgentId;
 
-    private MutableLiveData<String> mText;
-
-    public Customer() {
-        mText = new MutableLiveData<>();
-        mText.setValue("This is customer fragment");
+    public Customer(int customerId, String custFirstName, String custLastName, String custAddress,
+                    String custCity, String custProv, String custPostal, String custCountry,
+                    String custHomePhone, String custBusPhone, String custEmail, Integer agentId)
+    {
+        CustomerId = customerId;
+        CustFirstName = custFirstName;
+        CustLastName = custLastName;
+        CustAddress = custAddress;
+        CustCity = custCity;
+        CustProv = custProv;
+        CustPostal = custPostal;
+        CustCountry = custCountry;
+        CustHomePhone = custHomePhone;
+        CustBusPhone = custBusPhone;
+        CustEmail = custEmail;
+        AgentId = agentId;
     }
 
-    public LiveData<String> getText() {
-        return mText;
+    public int getCustomerId() {
+        return CustomerId;
+    }
+
+    public void setCustomerId(int customerId) {
+        CustomerId = customerId;
+    }
+
+    public String getCustFirstName() {
+        return CustFirstName;
+    }
+
+    public void setCustFirstName(String custFirstName) {
+        CustFirstName = custFirstName;
+    }
+
+    public String getCustLastName() {
+        return CustLastName;
+    }
+
+    public void setCustLastName(String custLastName) {
+        CustLastName = custLastName;
+    }
+
+    public String getCustAddress() {
+        return CustAddress;
+    }
+
+    public void setCustAddress(String custAddress) {
+        CustAddress = custAddress;
+    }
+
+    public String getCustCity() {
+        return CustCity;
+    }
+
+    public void setCustCity(String custCity) {
+        CustCity = custCity;
+    }
+
+    public String getCustProv() {
+        return CustProv;
+    }
+
+    public void setCustProv(String custProv) {
+        CustProv = custProv;
+    }
+
+    public String getCustPostal() {
+        return CustPostal;
+    }
+
+    public void setCustPostal(String custPostal) {
+        CustPostal = custPostal;
+    }
+
+    public String getCustCountry() {
+        return CustCountry;
+    }
+
+    public void setCustCountry(String custCountry) {
+        CustCountry = custCountry;
+    }
+
+    public String getCustHomePhone() {
+        return CustHomePhone;
+    }
+
+    public void setCustHomePhone(String custHomePhone) {
+        CustHomePhone = custHomePhone;
+    }
+
+    public String getCustBusPhone() {
+        return CustBusPhone;
+    }
+
+    public void setCustBusPhone(String custBusPhone) {
+        CustBusPhone = custBusPhone;
+    }
+
+    public String getCustEmail() {
+        return CustEmail;
+    }
+
+    public void setCustEmail(String custEmail) {
+        CustEmail = custEmail;
+    }
+
+    public Integer getAgentId() {
+        return AgentId;
+    }
+
+    public void setAgentId(Integer agentId) {
+        AgentId = agentId;
     }
 }
--- app/src/main/java/com/example/workshop8/ui/customers/CustomersFragment.java
+++ app/src/main/java/com/example/workshop8/ui/customers/CustomersFragment.java
@@ -4,6 +4,9 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -12,24 +15,93 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.VolleyLog;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.example.workshop8.R;
+import com.google.gson.Gson;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+
+import java.util.concurrent.Executors;
 
 public class CustomersFragment extends Fragment {
 
     private Customer customer;
+    RequestQueue requestQueue;
+
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         customer =
                 new ViewModelProvider(this).get(Customer.class);
-        View root = inflater.inflate(R.layout.fragment_gallery, container, false);
-        final TextView textView = root.findViewById(R.id.text_gallery);
-        customer.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+        View root = inflater.inflate(R.layout.fragment_customers, container, false);
+        final ListView lvCustomers = root.findViewById(R.id.lvCustomers);
+
+        requestQueue = Volley.newRequestQueue(this);
+
+        Executors.newSingleThreadExecutor().execute(new GetCustomers());
+
+        lvCustomers.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Customer c = (Customer) lvCustomers.getAdapter().getItem(position);
+                //apply setText to fields;
             }
         });
+
         return root;
+    }
+
+    private class GetCustomers implements Runnable {
+        @Override
+        public void run() {
+            String url = "http://localhost:8080/workshop7_war_exploded/customers/getallcustomers";
+            StringRequest stringRequest = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
+                @Override
+                public void onResponse(String response) {
+                    ArrayAdapter<Customer> adapter = new ArrayAdapter<>(getApplicationContext(). android.R.layout.simple_list_item_1);
+                    try {
+                        JSONArray jsonArray  = new JSONArray(response);
+//                        for (int i=0; 1 < jsonArray.length(); i++){
+//                            JSONObject cust = jsonArray.getJSONObject(i);
+//
+//                            Customer c = new Customer( cust.getInt("CustomerId"),
+//                                    cust.getString("CustFirstName"),
+//                                    cust.getString("CustLastName"),
+//                                    cust.getString("CustAddress"),
+//                                    cust.getString("CustCity"),
+//                                    cust.getString("CustProv"),
+//                                    cust.getString("CustPostal"),
+//                                    cust.getString("CustCountry"),
+//                                    cust.getString("CustHomePhone"),
+//                                    cust.getString("CustBusPhone"),
+//                                    cust.getString("CustEmail"),
+//                                    cust.getInt("AgentId"));
+//                            adapter.add(c);
+//                        }
+                        for (int i=0; 1 < jsonArray.length(); i++){
+                            Customer c = new Gson().fromJson(jsonArray.getString(i), Customer.class);
+                            adapter.add(c);
+                        }
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    VolleyLog.d(error.getMessage());
+                }
+            });
+
+            requestQueue.add(stringRequest);
+
+        }
     }
 }
